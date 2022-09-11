96ce669611f6ed9aa9627563a8a56f0a16141a32
bekarysibrashev <beka.ibrashev@gmail.com>
2022-09-11 17:53:34 +0500

    d

--- websitePO/mainpage/views.py
+++ websitePO/mainpage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Here is PO site")
+    return HttpResponse("Hello, Heredede is PO site")
 # Create your views here.
