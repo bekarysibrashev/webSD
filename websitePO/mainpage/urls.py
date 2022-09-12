1b9b001e793dabca469eff246a202dfd9db9a48b
bekarysibrashev <beka.ibrashev@gmail.com>
2022-09-12 21:53:52 +0500

    Added html patterns and new page

--- websitePO/mainpage/urls.py
+++ websitePO/mainpage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_group', views.about_group, name='about_group')
 ]
--- websitePO/mainpage/views.py
+++ websitePO/mainpage/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Heredede is PO site")
+    return render(request, 'mainpage/index.html')
 # Create your views here.
+
+def about_group(request):
+    return render(request, 'mainpage/about_group.html')
