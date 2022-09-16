527993fb20d9c0989807b74458164486ce2a01ac
bekarysibrashev <beka.ibrashev@gmail.com>
2022-09-16 22:52:37 +0500

    fr

--- websitePO/mainpage/admin.py
+++ websitePO/mainpage/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Participance
 
-# Register your models here.
+admin.site.register(Participance)
--- websitePO/mainpage/models.py
+++ websitePO/mainpage/models.py
@@ -1,3 +1,12 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Participance(models.Model):
+    name = models.CharField("Имя", max_length=50)
+    age = models.IntegerField("Возраст")
+    bio = models.TextField("Биография")
+
+    def __str__(self):
+        return self.title
