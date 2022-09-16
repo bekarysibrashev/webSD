a141033d27a0ecc960b26e05d29ff1ab5255a419
bekarysibrashev <beka.ibrashev@gmail.com>
2022-09-16 21:11:36 +0500

    d

---
+++ websitePO/news/models.py
@@ -0,0 +1,11 @@
+from email.quoprimime import body_check
+from django.db import models
+
+class Articles(models.Model):
+    title = models.CharField(max_length= 120)
+    post = models.TextField()
+    date = models.DateTimeField()
+
+def __str__(self):
+    return self.title
+
