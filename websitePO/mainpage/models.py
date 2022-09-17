a0e97e627e07083d642a3a362f707b191a43ac61
tasakbastar <beka.ibrashev@gmail.com>
2022-09-17 16:55:06 +0500

    for bekarys

--- websitePO/mainpage/models.py
+++ websitePO/mainpage/models.py
@@ -5,15 +5,7 @@ class Participance(models.Model):
     name = models.CharField("Имя", max_length=50)
     age = models.IntegerField("Возраст")
     bio = models.TextField("Биография")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
     def __str__(self):
         return self.name
->>>>>>> 2a9a21addfcb42eec5f427a0f317a630129a821f
-=======
 
-    def __str__(self):
-        return self.name
->>>>>>> refs/remotes/origin/main
