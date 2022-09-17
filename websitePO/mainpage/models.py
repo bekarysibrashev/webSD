c85e206c1dd053a0debf8493bf7ade59d288d4c6
tasakbastar <beka.ibrashev@gmail.com>
2022-09-17 15:55:18 +0500

    ffrr

--- websitePO/mainpage/models.py
+++ websitePO/mainpage/models.py
@@ -4,11 +4,4 @@ from django.db import models
 class Participance(models.Model):
     name = models.CharField("Имя", max_length=50)
     age = models.IntegerField("Возраст")
-<<<<<<< HEAD
     bio = models.TextField("Биография")
-=======
-    bio = models.TextField("Биография")
-
-    def __str__(self):
-        return self.name
->>>>>>> 5c289930fa289b35b380c67cdcac6f4ad2f76888
