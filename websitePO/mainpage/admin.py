91a0efdbcc161fe8cefec849bda8e35447d95ffd
bekarysibrashev <beka.ibrashev@gmail.com>
2022-09-18 20:09:13 +0500

    dd

--- websitePO/mainpage/admin.py
+++ websitePO/mainpage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Participance
+from .models import Participance, NewText
 
 admin.site.register(Participance)
-
+admin.site.register(NewText)
--- websitePO/mainpage/models.py
+++ websitePO/mainpage/models.py
@@ -9,3 +9,9 @@ class Participance(models.Model):
     def __str__(self):
         return self.name
 
+class NewText(models.Model):
+    topic = models.CharField("Тема", max_length=50)
+    info = models.CharField("Заголовок", max_length=1000)
+
+    def __str__(self):
+        return self.topic
